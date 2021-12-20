e2f4cd80f05251dad5089b8d139276a69059fb66
Filip Dabkowski <filip.dabkowski@gmail.com>
2021-12-20 22:46:32 +0100

    homework 2

---
+++ src/lesson2/homework.py
@@ -0,0 +1,21 @@
+# 0. nie mowie ze kod z neta jest zly. tez tak ciagle robie. ale bede kazal go sobie wytlumaczyc ;)
+
+# 1. przeiteruj listę `animals` (odczytaj każdy element po kolei) wynik pokaz w konsoli (wyprintuj)
+#       lista nie ma stalej dlugosci zabraniam urzywac range(n) n - liczba wpisana z palca
+animals = ['dog', 'bird', 'cat', 'monkey', 'elephant', 'pikachu', 'snake']
+
+
+# 2. jesli aktualnie sprawdzane zwieze to np 'snake' print 'cutie' dalej jesli 'cat' print 'blleleeee' jesli zadne z
+#       tych wyprintuj normalne zwierze
+
+
+# 3. (uwaga bedzie myslenie) sprawdz czy wartosc w zmiennej `usrname` znajduje sie juz w liscie `database` jesli tak
+#       wyprintuj 'this username already exists' i wywal go z tabeli jesli nie print 'congrats interesting choice' i
+#       dodaj go do `database`
+username = 'filip123'
+database = ['xosiaxo', 'franchesho', 'filip123']
+
+
+# 4. ponizej jest tablica z wynikami z modelu (AI) pokaz 3 najwyzsze winiki oraz 2 najnizsze i podajce na ktorej pozycji
+#       stoi w liscie
+predictions = [95.345, 63.8752, 12.53, 6.48, 2.03, 78.34, 0.557, 6.457, 16.21, 12.26, 91.245, 0.123, 14.14, 14.35]
